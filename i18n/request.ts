3cb1449a10f4dd906241c4b5cbe36216e21c5128
Thor Lin <coolthor@gmail.com>
2026-01-11 20:41:30 +0800

    fixed for vercel

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,8 +1,10 @@
 import { getRequestConfig } from 'next-intl/server';
 
-export default getRequestConfig(async ({ locale }) => {
+export default getRequestConfig(async ({ requestLocale }) => {
   // Using a dynamic import for the messages is recommended.
   // This will only load the messages for the given locale.
+  const locale = await requestLocale;
+
   return {
     locale,
     messages: (await import(`../messages/${locale}.json`)).default
