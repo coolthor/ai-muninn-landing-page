f5fc98c23a7f258c44feaddea1422ada500a7f3d
Thor Lin <coolthor@gmail.com>
2026-01-11 20:43:40 +0800

    改善vercel 的build

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,9 +1,15 @@
 import { getRequestConfig } from 'next-intl/server';
+import { routing } from './routing';
 
 export default getRequestConfig(async ({ requestLocale }) => {
   // Using a dynamic import for the messages is recommended.
   // This will only load the messages for the given locale.
-  const locale = await requestLocale;
+  let locale = await requestLocale;
+
+  // Ensure that a valid locale is used
+  if (!locale || !routing.locales.includes(locale as any)) {
+    locale = routing.defaultLocale;
+  }
 
   return {
     locale,
